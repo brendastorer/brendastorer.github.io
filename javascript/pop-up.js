4739d88eb96f061bca67cd786e669969ae989cfc
Brenda Storer <brendastorer@gmail.com>
2016-09-03 17:24:43 -0400

    Add podcasts to website

--- javascript/pop-up.js
+++ javascript/pop-up.js
@@ -1,13 +1,19 @@
 $(function() {
-  $(".js-presentation-button").on( "click", function(event) {
+  $(".js-presentations__open").on("click", function(event) {
     event.preventDefault();
-    $(".js-pop-up").addClass("show");
+    $(".js-presentations").addClass("show");
     $(".js-main").addClass("blur");
   });
 
-  $(".js-pop-up__close").on( "click", function(event) {
+  $(".js-podcasts__open").on("click", function(event) {
     event.preventDefault();
-    $(".js-pop-up").removeClass("show");
+    $(".js-podcasts").addClass("show");
+    $(".js-main").addClass("blur");
+  });
+
+  $(".js-pop-up__close").on("click", function(event) {
+    event.preventDefault();
+    $(".js-presentations, .js-podcasts").removeClass("show");
     $(".js-main").removeClass("blur");
   });
 });
