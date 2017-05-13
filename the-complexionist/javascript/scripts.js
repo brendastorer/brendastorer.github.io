a955ee0ad45284068abcfa8c6cf066f4bab08d7d
Brenda Storer <brendastorer@gmail.com>
2017-05-13 11:56:14 -0400

    Mobile menu for complexionist site

--- the-complexionist/javascript/scripts.js
+++ the-complexionist/javascript/scripts.js
@@ -1,4 +1,19 @@
-window.addEventListener('mouseover', function onFirstHover() {
-  document.body.classList.add('user-can-hover');
+// Detect touch device
+
+window.addEventListener('touchstart', function onFirstTouch() {
+  document.body.classList.add('user-has-touch');
   window.removeEventListener('mouseover', onFirstHover, false);
 }, false);
+
+// Mobile Menu
+$(function() {
+  $(".js-mobile-nav__trigger").click(function(event){
+    event.preventDefault();
+    $(".js-site-nav__list").addClass("open");
+  });
+
+  $(".js-mobile-nav__close").click(function(event){
+    event.preventDefault();
+    $(".js-site-nav__list").removeClass("open");
+  });
+});
