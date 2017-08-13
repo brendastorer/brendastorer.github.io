abcb088337f4414588c7603ecf2cec9b7078d415
Brenda Storer <brendastorer@gmail.com>
2017-08-13 14:38:01 -0400

    More modular javascript

--- abba-the-quiz/quiz.js
+++ abba-the-quiz/quiz.js
@@ -74,14 +74,14 @@ const QUIZ = [
   {
     question: "What is the name of ABBA’s self written mini operetta?",
     option1: "Voulez-Vous",
-    option2: "The Girl with the Golden Hair",
+    option2: "Super Trouper",
     option3: "Thank You For the Music",
-    option4: "Super Trouper",
+    option4: "The Girl with the Golden Hair",
     answer: "The Girl with the Golden Hair"
   }
 ];
 
-let CURRENT_QUESTION = 0;
+let CURRENT_QUESTION_INDEX = 0;
 let CORRECT_ANSWER_COUNT = 0;
 let QUESTIONS_ANSWERED = 0;
 const TOTAL_QUESTIONS = QUIZ.length;
@@ -92,53 +92,60 @@ const QUIZ_FORM_ID = "js-quiz-form";
 const HIDDEN_ELEMENTS = ".js-hide-while-taking-quiz";
 const QUIZ_INPUT_CLASS = "js-quiz-input";
 const QUIZ_OPTION_CLASS = "js-quiz-option";
+const QUIZ_QUESTION_COUNTER = ".js-question-count";
+const QUIZ_SCORE = ".js-score";
+const QUIZ_LOAD_ACTION = ".js-quiz-start";
+const QUIZ_NEXT_ACTION = ".js-next-question";
 
-function renderQuestion() {
-  const currentQuestion = QUIZ[CURRENT_QUESTION];
-  const currentQuestionCount = CURRENT_QUESTION + 1;
+function renderQuestionCount() {
+  const currentQuestionCount = CURRENT_QUESTION_INDEX + 1;
 
   return `
-    <header class="quiz-section__header">
-      <h2>
-        Question ${currentQuestionCount} / ${TOTAL_QUESTIONS}
-      </h2>
-      <h2>
-        Score ${CORRECT_ANSWER_COUNT} / ${QUESTIONS_ANSWERED}
-      </h2>
-    </header>
-    <form class="quiz-form" id="${QUIZ_FORM_ID}">
-      <h3 class="quiz-form__question">
-        ${currentQuestion.question}
-      </h3>
-      <input class="quiz-form__input ${QUIZ_INPUT_CLASS}" type="radio" name="question" id="option-1" value="${currentQuestion.option1}">
-      <label class="quiz-form__option ${QUIZ_OPTION_CLASS}" for="option-1">
-        <span class="quiz-form__option-answer">
-          ${currentQuestion.option1}
-        </span>
-      </label>
+    Question ${currentQuestionCount} / ${TOTAL_QUESTIONS}
+  `
+}
 
-      <input class="quiz-form__input ${QUIZ_INPUT_CLASS}" type="radio" name="question" id="option-2" value="${currentQuestion.option2}">
-      <label class="quiz-form__option ${QUIZ_OPTION_CLASS}" for="option-2">
-        <span class="quiz-form__option-answer">
-          ${currentQuestion.option2}
-        </span>
-      </label>
+function renderScore() {
+  return `
+    Score ${CORRECT_ANSWER_COUNT} / ${QUESTIONS_ANSWERED}
+  `
+}
 
-      <input class="quiz-form__input ${QUIZ_INPUT_CLASS}" type="radio" name="question" id="option-3" value="${currentQuestion.option3}">
-      <label class="quiz-form__option ${QUIZ_OPTION_CLASS}" for="option-3">
-        <span class="quiz-form__option-answer">
-          ${currentQuestion.option3}
-        </span>
-      </label>
+function renderQuestion() {
+  const currentQuestion = QUIZ[CURRENT_QUESTION_INDEX];
 
-      <input class="quiz-form__input ${QUIZ_INPUT_CLASS}" type="radio" name="question" id="option-4" value="${currentQuestion.option4}">
-      <label class="quiz-form__option ${QUIZ_OPTION_CLASS}" for="option-4">
-        <span class="quiz-form__option-answer">
-          ${currentQuestion.option4}
-        </span>
-      </label>
-      <button type="submit" class="hidden"></button>
-    </form>
+  return `
+    <h3 class="quiz-form__question">
+      ${currentQuestion.question}
+    </h3>
+    <input class="quiz-form__input ${QUIZ_INPUT_CLASS}" type="radio" name="question" id="option-1" value="${currentQuestion.option1}">
+    <label class="quiz-form__option ${QUIZ_OPTION_CLASS}" for="option-1">
+      <span class="quiz-form__option-answer">
+        ${currentQuestion.option1}
+      </span>
+    </label>
+
+    <input class="quiz-form__input ${QUIZ_INPUT_CLASS}" type="radio" name="question" id="option-2" value="${currentQuestion.option2}">
+    <label class="quiz-form__option ${QUIZ_OPTION_CLASS}" for="option-2">
+      <span class="quiz-form__option-answer">
+        ${currentQuestion.option2}
+      </span>
+    </label>
+
+    <input class="quiz-form__input ${QUIZ_INPUT_CLASS}" type="radio" name="question" id="option-3" value="${currentQuestion.option3}">
+    <label class="quiz-form__option ${QUIZ_OPTION_CLASS}" for="option-3">
+      <span class="quiz-form__option-answer">
+        ${currentQuestion.option3}
+      </span>
+    </label>
+
+    <input class="quiz-form__input ${QUIZ_INPUT_CLASS}" type="radio" name="question" id="option-4" value="${currentQuestion.option4}">
+    <label class="quiz-form__option ${QUIZ_OPTION_CLASS}" for="option-4">
+      <span class="quiz-form__option-answer">
+        ${currentQuestion.option4}
+      </span>
+    </label>
+    <button type="submit" class="quiz-form__button">Submit</button>
   `;
 }
 
@@ -148,7 +155,6 @@ function renderResults() {
       <h2 class="non-quiz-section__title">Score ${CORRECT_ANSWER_COUNT} / ${QUESTIONS_ANSWERED}</h2>
       <h3 class="non-quiz-section__info">Great job! You truly are a dancing queen!</h3>
       <img src="images/abba-dancing-queen.gif" alt='A silent clip of Frida performing Dancing Queen with subtitles of the lyrics "Dancing Queen feel the beat from the tambourine"' class="non-quiz-section__image">
-      <button type="reset" class="js-restart">Try again</button>
     `;
   }
   else {
@@ -156,87 +162,131 @@ function renderResults() {
       <h2 class="non-quiz-section__title">Score ${CORRECT_ANSWER_COUNT} / ${QUESTIONS_ANSWERED}</h2>
       <h3 class="non-quiz-section__info">Sorry. You are the loser in this melody. Better luck next time.</h3>
       <img src="images/abba-loser-fall.gif" alt='A silent clip of Agnethe singing The Winner Takes It All with subtitles of the lyrics "The winner takes it all, the loser has to fall."' class="non-quiz-section__image">
-      <button type="reset" class="js-restart">Try again</button>
     `;
   }
 }
 
+function showQuestionCount() {
+  $(QUIZ_QUESTION_COUNTER).empty().prepend(renderQuestionCount);
+}
+
+function showScore() {
+  $(QUIZ_SCORE).empty().prepend(renderScore);
+}
+
 function showQuestion() {
-  $(QUIZ_QUESTION).empty().prepend(renderQuestion);
-  evaluateAnswer();
-  submitOnClick();
+  $(`#${QUIZ_FORM_ID}`).empty().prepend(renderQuestion);
 }
 
 function showResults() {
   $(QUIZ_SECTION).hide();
   $(QUIZ_RESULTS).show().prepend(renderResults);
-  $(restartQuiz);
+  CURRENT_QUESTION_INDEX = 0;
+  CORRECT_ANSWER_COUNT = 0;
+  QUESTIONS_ANSWERED = 0;
 }
 
-function evaluateAnswer() {
-  $(`#${QUIZ_FORM_ID}`).submit(function(event) {
-    event.preventDefault();
-    const currentQuestion = QUIZ[CURRENT_QUESTION];
-    const answer = $(this).find(`.${QUIZ_INPUT_CLASS}:checked`);
-    const answerValue = answer.val();
-
-    if (answerValue === currentQuestion.answer) {
-      $(answer).addClass("correct-answer");
-      $(answer).next(`.${QUIZ_OPTION_CLASS}`).append("<span class='quiz-form__response'>correct!</span>");
-      CORRECT_ANSWER_COUNT++;
-    }
-    else {
-      // also display correct answer
-      $(answer).addClass("wrong-answer");
-      $(answer).next(`.${QUIZ_OPTION_CLASS}`).append("<span class='quiz-form__response'>wrong!</span>");
-    }
-
-    QUESTIONS_ANSWERED++;
-    nextStep(CURRENT_QUESTION);
-    $(this).reset();
-  });
-}
-
-function submitOnClick() {
-  $(`.${QUIZ_OPTION_CLASS}`).on("click", function() {
-    $(this).prev(`.${QUIZ_INPUT_CLASS}`).attr("checked", true);
-    $(`#${QUIZ_FORM_ID}`).submit();
-  });
-}
-
-function nextStep(index) {
-  const timeBetweenSteps = 1500;
+function increaseQuestionCount(index) {
+  event.preventDefault();
 
   if (QUESTIONS_ANSWERED === TOTAL_QUESTIONS) {
-    setTimeout(showResults, timeBetweenSteps);
+    showResults();
   } 
   else {
     index++;
   }
 
-  CURRENT_QUESTION = index;
-  setTimeout(showQuestion, timeBetweenSteps);
+  CURRENT_QUESTION_INDEX = index;
 }
 
-function restartQuiz() {
-  $(".js-restart").on("click", function(event){
-    event.preventDefault;
-    CURRENT_QUESTION = 0;
-    CORRECT_ANSWER_COUNT = 0;
-    QUESTIONS_ANSWERED = 0;
-    $(HIDDEN_ELEMENTS).hide();
-    $(QUIZ_SECTION).show();
-    $(QUIZ_RESULTS).empty();
-    $(showQuestion);
+function findCorrectAnswer() {
+  const currentQuestion = QUIZ[CURRENT_QUESTION_INDEX];
+  const possibleAnswers = Object.values(currentQuestion).slice(1, 5);
+  let correctAnswerIndex = "";
+
+  for (let i = 0; i < possibleAnswers.length; ++i) {
+    if (possibleAnswers[i] === currentQuestion.answer) {
+      correctAnswerIndex = i;
+    }
+  }
+  return correctAnswerIndex;
+}
+
+function showCorrectAnswer() {
+  const correctAnswerIndex = findCorrectAnswer() + 1;
+  const correctAnswer = $(`.${QUIZ_INPUT_CLASS}:nth-of-type(${correctAnswerIndex})`);
+  const correctInsert = "<span class='quiz-form__response'>correct!</span>"
+
+  $(correctAnswer).addClass("correct-answer").next(`.${QUIZ_OPTION_CLASS}`).append(correctInsert);
+}
+
+function evaluateAnswer() {
+  const currentQuestion = QUIZ[CURRENT_QUESTION_INDEX];
+  const answer = $(`.${QUIZ_INPUT_CLASS}:checked`);
+  const answerValue = answer.val();
+
+  if (answerValue === currentQuestion.answer) {
+    CORRECT_ANSWER_COUNT++;
+  }
+  else {
+    $(answer).addClass("wrong-answer");
+    $(answer).next(`.${QUIZ_OPTION_CLASS}`).append("<span class='quiz-form__response'>wrong!</span>");
+  }
+
+  QUESTIONS_ANSWERED++;
+}
+
+function clearForm() {
+  const form = document.getElementById(QUIZ_FORM_ID);
+  form.reset();
+}
+
+function disableFormSubmission() {
+  $(`#${QUIZ_FORM_ID}`).find(':input[type=submit]').prop('disabled', true);
+}
+
+function submitAnswer() {
+  $(`#${QUIZ_FORM_ID}`).submit(function(event) {
+    event.preventDefault();
+
+    $(QUIZ_NEXT_ACTION).show();
+    disableFormSubmission();
+    showCorrectAnswer();
+    evaluateAnswer();
+    showNextButton();
   });
 }
 
-function loadPage() {
-  $(".js-quiz-start").on("click", function(event) {
-    $(HIDDEN_ELEMENTS).hide();
-    $(QUIZ_SECTION).show();
-    $(showQuestion);
+function loadQuestion() {
+  $(QUIZ_NEXT_ACTION).on("click", function(event){
+    event.preventDefault();
+    
+    $(QUIZ_NEXT_ACTION).hide();
+    clearForm();
+    increaseQuestionCount(CURRENT_QUESTION_INDEX);
+    showQuestionCount();
+    showScore();
+    showQuestion();
   });
 }
 
-$(loadPage);
+function startQuiz() {
+  $(QUIZ_LOAD_ACTION).on("click", function(event){
+    event.preventDefault();
+
+    $(HIDDEN_ELEMENTS).hide();
+    $(QUIZ_SECTION).show();
+    showQuestionCount();
+    showScore();
+    showQuestion();
+  });
+}
+
+function theQuiz() {
+  startQuiz();
+  findCorrectAnswer();
+  submitAnswer();
+  loadQuestion();
+}
+
+$(theQuiz);
