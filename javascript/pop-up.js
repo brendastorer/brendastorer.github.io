33f4c8a0844518bfc6a4407585270ebc29b62485
Brenda Storer <brendastorer@gmail.com>
2017-10-18 19:35:34 -0400

    Make my landing page links tabbable based on what it is view

--- javascript/pop-up.js
+++ javascript/pop-up.js
@@ -3,17 +3,20 @@ $(function() {
     event.preventDefault();
     $(".js-presentations").addClass("show");
     $(".js-main").addClass("blur");
+    $(".js-icon-nav-link").addClass("hide");
   });
 
   $(".js-podcasts__open").on("click", function(event) {
     event.preventDefault();
     $(".js-podcasts").addClass("show");
     $(".js-main").addClass("blur");
+    $(".js-icon-nav-link").addClass("hide");
   });
 
   $(".js-pop-up__close").on("click", function(event) {
     event.preventDefault();
     $(".js-presentations, .js-podcasts").removeClass("show");
     $(".js-main").removeClass("blur");
+    $(".js-icon-nav-link").removeClass("hide");
   });
 });
